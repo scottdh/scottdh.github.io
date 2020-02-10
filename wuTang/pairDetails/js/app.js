afa820dd1255cb411cfddad056237181748684ac
Scottd_h <scott.herrington@me.com>
2020-02-10 14:52:08 +0000

    updated with responsive design

--- wuTang/pairDetails/js/app.js
+++ wuTang/pairDetails/js/app.js
@@ -1,75 +1,75 @@
-// var bookmark = document.querySelector("#bookmark");
-var additionalInfoRight = document.querySelector("#additionalRight");
-var additionalInfoLeft = document.querySelector("#additionalLeft");
+// // var bookmark = document.querySelector("#bookmark");
+// var additionalInfoRight = document.querySelector("#additionalRight");
+// var additionalInfoLeft = document.querySelector("#additionalLeft");
 
-// bookmark.addEventListener(
-//   "mouseover",
-//   function() {
-//     // if (!e.target === compareBox) return;
-//     additionalInfoRight.classList.add("right");
-//     additionalInfoLeft.classList.add("left");
-//   },
-//   false
-// );
+// // bookmark.addEventListener(
+// //   "mouseover",
+// //   function() {
+// //     // if (!e.target === compareBox) return;
+// //     additionalInfoRight.classList.add("right");
+// //     additionalInfoLeft.classList.add("left");
+// //   },
+// //   false
+// // );
 
-// bookmark.addEventListener(
-//   "mouseout",
-//   function() {
-//     additionalInfoRight.classList.remove("right");
-//     additionalInfoLeft.classList.remove("left");
-//   },
-//   false
-// );
+// // bookmark.addEventListener(
+// //   "mouseout",
+// //   function() {
+// //     additionalInfoRight.classList.remove("right");
+// //     additionalInfoLeft.classList.remove("left");
+// //   },
+// //   false
+// // );
 
-// window.addEventListener()
+// // window.addEventListener()
 
-var group = document.querySelector("#bookmark");
-var nodes = document.querySelectorAll(".box");
-var total = nodes.length;
-var ease = Power1.easeInOut;
-var boxes = [];
+// var group = document.querySelector("#bookmark");
+// var nodes = document.querySelectorAll(".box");
+// var total = nodes.length;
+// var ease = Power1.easeInOut;
+// var boxes = [];
 
-for (var i = 0; i < total; i++) {
-  var node = nodes[i];
+// for (var i = 0; i < total; i++) {
+//   var node = nodes[i];
 
-  // Initialize transforms on node
-  TweenLite.set(node, { x: 0 });
+//   // Initialize transforms on node
+//   TweenLite.set(node, { x: 0 });
 
-  boxes[i] = {
-    transform: node._gsTransform,
-    x: node.offsetLeft,
-    y: node.offsetTop,
-    node
-  };
-}
+//   boxes[i] = {
+//     transform: node._gsTransform,
+//     x: node.offsetLeft,
+//     y: node.offsetTop,
+//     node
+//   };
+// }
 
-bookmark.addEventListener("mouseenter", layout);
-bookmark.addEventListener("mouseleave", layout);
+// bookmark.addEventListener("mouseenter", layout);
+// bookmark.addEventListener("mouseleave", layout);
 
-function layout() {
-  console.log("moo");
-  additionalInfoLeft.classList.toggle("reorder");
-  additionalInfoRight.classList.toggle("reorder");
-  additionalInfoLeft.classList.toggle("left");
-  additionalInfoRight.classList.toggle("right");
+// function layout() {
+//   console.log("moo");
+//   additionalInfoLeft.classList.toggle("reorder");
+//   additionalInfoRight.classList.toggle("reorder");
+//   additionalInfoLeft.classList.toggle("left");
+//   additionalInfoRight.classList.toggle("right");
 
-  for (var i = 0; i < total; i++) {
-    var box = boxes[i];
+//   for (var i = 0; i < total; i++) {
+//     var box = boxes[i];
 
-    var lastX = box.x;
-    var lastY = box.y;
+//     var lastX = box.x;
+//     var lastY = box.y;
 
-    box.x = box.node.offsetLeft;
-    box.y = box.node.offsetTop;
+//     box.x = box.node.offsetLeft;
+//     box.y = box.node.offsetTop;
 
-    // Continue if box hasn't moved
-    if (lastX === box.x && lastY === box.y) continue;
+//     // Continue if box hasn't moved
+//     if (lastX === box.x && lastY === box.y) continue;
 
-    // Reversed delta values taking into account current transforms
-    var x = box.transform.x + lastX - box.x;
-    var y = box.transform.y + lastY - box.y;
+//     // Reversed delta values taking into account current transforms
+//     var x = box.transform.x + lastX - box.x;
+//     var y = box.transform.y + lastY - box.y;
 
-    // Tween to 0 to remove the transforms
-    TweenLite.fromTo(box.node, 0.5, { x, y }, { x: 0, y: 0, ease });
-  }
-}
+//     // Tween to 0 to remove the transforms
+//     TweenLite.fromTo(box.node, 0.5, { x, y }, { x: 0, y: 0, ease });
+//   }
+// }
